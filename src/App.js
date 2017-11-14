e9c2254a60dbcda1f1b9cc41ef9961db1263828a
Danny Eck <daniel.eck@accenture.com>
2017-11-13 20:38:34 -0800

    Add components to kitchen sink

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Row,
   Col,
   Button,
+  Badge
 } from 'cwds-components';
 import 'cwds-components/dist/style.css';
 
@@ -31,15 +32,25 @@ class App extends Component {
         <Alert type="info"><strong>Info</strong> Just an FYI...</Alert>
         <Alert type="warning"><strong>Warning</strong> Be careful!</Alert>
         <Alert type="danger"><strong>Danger</strong> Oh nos! Something broke!</Alert>
+        <Alert type="danger" onDismiss={dismissAlert}><strong>Danger</strong> Oh nos! Dismiss this alert before my boss sees it</Alert>
         {/* <Alert type="light"><strong>Light</strong> I dunno...</Alert> */}
         {/* <Alert type="dark"><strong>dark</strong> I dunno...</Alert> */}
 
-        <Alert type="info">
+        <Alert type="info" onDismiss={dismissAlert}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et magna ut risus sollicitudin dapibus at nec mi. Fusce magna augue, tristique vitae velit ac, dictum iaculis turpis. Nullam blandit tincidunt enim, eget porta odio dapibus vel. Fusce quis molestie erat, ac malesuada massa. Curabitur et dui id turpis finibus ullamcorper. Morbi tincidunt mattis metus, non ultricies odio porttitor ac. Ut dictum nunc id ex iaculis, eu tempus orci rutrum. Aliquam aliquam tortor at magna viverra venenatis. Sed aliquet pellentesque tristique. Maecenas molestie commodo orci, id feugiat ligula consequat a. Sed auctor quam vel scelerisque sodales. Duis aliquet lectus ex.
         </Alert>
 
-        <h2>Buttons</h2>
+        <h2>Badges</h2>
+        <Badge color="primary">Primary</Badge>{' '}
+        <Badge color="secondary">Secondary</Badge>{' '}
+        <Badge color="success">Success</Badge>{' '}
+        <Badge color="danger">Danger</Badge>{' '}
+        <Badge color="warning">Warning</Badge>{' '}
+        <Badge color="info">Info</Badge>{' '}
+        <Badge color="light">Light</Badge>{' '}
+        <Badge color="dark">Dark</Badge>{' '}
 
+        <h2>Buttons</h2>
         <Button type="primary">Primary</Button>{' '}
         <Button type="secondary">Secondary</Button>{' '}
         <Button type="success">Success</Button>{' '}
@@ -49,9 +60,29 @@ class App extends Component {
         <Button type="light">Light</Button>{' '}
         {/* <Button type="dark">Dark</Button>{' '} */}
         <Button type="link">Link</Button>
-        
+
         <h2>Cards</h2>
-        <Card />
+        <Card>
+          <div className="card-body">I am a <tt>Card</tt></div>
+        </Card>
+        <br />
+        <div className="card">
+          <div className="card-header">
+            Card Header
+            <div className="card-actions">
+              <Button type="link" size="sm">Edit</Button>
+            </div>
+          </div>
+          <div className="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac fermentum elit, quis imperdiet nunc. Sed varius, urna eget lobortis consequat, massa sem sollicitudin eros, a dignissim justo libero bibendum libero. Phasellus et nibh neque. Fusce congue tempor mauris, a bibendum diam consectetur quis. Mauris eget mollis lectus. Nunc varius porttitor nisi, eu dapibus ipsum bibendum eget. Aliquam libero ante, fringilla non tristique nec, porttitor a lacus. Duis convallis odio quis ex porta, ut posuere justo feugiat. In hac habitasse platea dictumst.
+          </div>
+          <div className="card-footer text-right">
+            <div className="card-actions">
+              <Button type="link" size="sm">Cancel</Button>
+              <Button type="primary" size="sm">Save</Button>
+            </div>
+          </div>
+        </div>
 
         <h2>Grid</h2>
 
