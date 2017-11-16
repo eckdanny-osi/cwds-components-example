dafdb12ab87172bb378c4818b3f3525f883cebb0
Danny Eck <daniel.eck@accenture.com>
2017-11-16 10:22:00 -0800

    Update example build

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import KitchenSink from './PageKitchenSink';
 import Home from './PageHome';
 import PageNav from './PageNav';
 
+const Page404 = () => (
+  <h2>404, yo!</h2>
+);
+
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <div>
       <Route exact path="/" component={KitchenSink} />
       {/* <Route path="/kitchen-sink" render={() => <h2>sink</h2>} /> */}
       <Route path="/home" component={Home} />
       <Route path="/nav" component={PageNav} />
+      <Route path='*' component={Page404} />
     </div>
   </Router>
 );
